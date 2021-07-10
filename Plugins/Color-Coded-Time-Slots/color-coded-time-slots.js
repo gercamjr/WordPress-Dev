45bc57f884a6767010b2ff15fc7ec4ce6c0749f2
Gerardo Camorlinga Jr <83178346+gercamjr@users.noreply.github.com>
2021-07-10 17:24:56 -0500

    Added files for the wp plugin
    color coded time slots plugin

---
+++ Plugins/Color-Coded-Time-Slots/color-coded-time-slots.js
@@ -0,0 +1,74 @@
+var jq = jQuery.noConflict();
+jq(document).ready(function() {
+    console.log("ready!");
+
+    // get the month and year
+    jq("#amelia-app-booking0").click(function() {
+        console.log("handled the service being clicked quite well");
+        var datArr = jq(".c-title").text().split(" ");
+        var month = datArr[16];
+        var year = datArr[17].substring(0, 4);
+        console.log(year);
+        console.log(month);
+
+        // turn month name into its corresponding MM format number
+        var bookingStart = (function() {
+            var months = {
+                "January": "01",
+                "February": "02",
+                "March": "03",
+                "April": "04",
+                "May": "05",
+                "June": "06",
+                "July": "07",
+                "August": "08",
+                "September": "09",
+                "October": "10",
+                "November": "11",
+                "December": "12"
+            };
+            var mm = months[month];
+            var formattedDate = year.concat("-").concat(mm).concat("-");
+            // should end up gettin something like "2021-04-""
+            return formattedDate;
+        })();
+        console.log(bookingStart)
+        var day = "";
+        //find out which day was picked 
+        jq(".c-day-content").click(function() {
+            bookingStart = bookingStart.substring(0, 8);
+            console.log("handled the day being picked quite nicely...");
+            day = jq(this).text();
+            day = day.replace(/[^0-9]/g, '');
+            console.log("day=" + day);
+            bookingStart = bookingStart.concat(day);
+            //query the db looking for time slots with appointments and returning this array
+            var obj = { action: "retrieveAttendees", dateSelected: bookingStart };
+            var postData = JSON.stringify(obj);
+            console.log(postData);
+            console.log(myAjax.ajaxurl);
+            nonce = jq(this).attr("data-nonce");
+            jq.ajax({
+                type: "POST",
+                dataType: "json",
+                url: myAjax.ajaxurl,
+                data: {
+                    action: "color-coded-time-slots",
+                    dateSelected: bookingStart,
+                    nonce: nonce
+                },
+                success: function(response) {
+                    console.log("we are in the callback");
+                    console.log(JSON.stringify(response));
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    console.log("we failed again: " + JSON.stringify(XMLHttpRequest));
+                    console.log("text status: " + textStatus);
+                    console.log("errorThrown: " + errorThrown)
+                }
+            });
+            console.log("exited the click handler");
+            //bookingStart = bookingStart.substring(0, 8);
+        });
+    });
+});
---
+++ Plugins/Color-Coded-Time-Slots/color-coded-time-slots.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Plugin Name: Color Coded Time Slots
+ * Plugin URI: http://geracomdev.com
+ * Description: This plugin color codes Amelia Booking time slots depending on number of attendees
+ * Version: 1.0.0
+ * Author: Gerardo Camorlinga Jr
+ * Author URI: http://geracomdev.com
+ * License: GPL2
+ */
+// Fires after WordPress has finished loading, but before any headers are sent.
+add_action( 'init', 'script_enqueuer' );
+add_action( 'wp_ajax_color-coded-time-slots', 'change_Colors' );
+add_action( 'wp_ajax_nopriv_color-coded-time-slots', 'change_Colors' );
+
+/** @return never  */
+function change_Colors() {
+    log("made it to the ajax request");
+	if ( isset($_REQUEST)) {
+		$bookingDay = $_REQUEST['dateSelected'];
+		global $wpdb;
+		$sql = $wpdb->prepare("select apps.bookingStart, COUNT(*) as booked from wp_amelia_customer_bookings as books inner join wp_amelia_appointments as apps on books.appointmentId = apps.id where apps.bookingStart like %s'%' and apps.status = 'approved' GROUP BY books.appointmentId;", $bookingDay);
+		$result = $wpdb->get_row($sql);
+		echo json_encode($result);
+	}
+	die();
+}
+
+/** @return void  */
+function script_enqueuer() {
+   
+   // Register the JS file with a unique handle, file location, and an array of dependencies
+   wp_register_script( "color-coded-time-slots", plugin_dir_url(__FILE__).'color-coded-time-slots.js', array('jquery') );
+   
+   // localize the script to your domain name, so that you can reference the url to admin-ajax.php file easily
+   wp_localize_script( 'color-coded-time-slots', 'myAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));        
+   
+   // enqueue jQuery library and the script you registered above
+   wp_enqueue_script( 'jquery' );
+   wp_enqueue_script( 'color-coded-time-slots' );
+}
+?>
